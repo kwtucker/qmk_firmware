47eb0ee7eafba52f54f77ad1a8df138e4a014844
kwtucker <kwtucker4@gmail.com>
2019-03-30 09:07:48 -0600

    update Keymap layout comment

--- keyboards/thevankeyboards/minivan/keymaps/kwtucker4/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/kwtucker4/keymap.c
@@ -12,6 +12,11 @@ extern keymap_config_t keymap_config;
 #define _L2 2
 #define _L3 3
 
+
+enum macro_keycodes {
+  KC_CMD_TAB,
+};
+
 // Macro name shortcuts
 #define QWERTY M(_QW)
 
@@ -19,35 +24,85 @@ extern keymap_config_t keymap_config;
 // the grid in layer 2.
 #define L_CURBR LSFT(KC_LBRC)
 #define R_CURBR LSFT(KC_RBRC)
+#define SFT_ESC SFT_T(KC_ESC)  // Tap for Escape, hold for Shift
+#define CMD_TAB     M(KC_CMD_TAB)               // Macro for Cmd-Tab
 
-    // K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B,
-    // K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B,
-    // K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,
-    // K30, K31, K32,           K33, K37,           K38, K39, K3B
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /* Qwerty
+  *
+  * ,---------+------+------+------+------+------+------+------+------+------+------+-------------.
+  * |   Tab   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   Bksp      |
+  * |---------`------`------`------`------`------`------`------`------`------`------`-------------|
+  * |   ML_2   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |    ML_2    |
+  * |----------`------`------`------`------`------`------`------`------`------`------`------------|
+  * | Shift/Esc |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | GUI/Space |
+  * |-----------`------`------`------`------`-----'-------`------`------`------`------`-----------|
+  * |   Ctrl  |   ML_3  |   GUI   |      Space     |     Enter      |   TG_1  |   TG_2  |  TG_3   |
+  *  `--------+---------+---------+------^^^-------+-----^^^--------+---------+---------+---------'
+  */
   [_QW] = LAYOUT( /* Qwerty */
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     MO(_L1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MO(_L2),
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RGUI(KC_SPC),
+    SFT_ESC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RGUI(KC_SPC),
     KC_LCTL, MO(_L3), KC_LGUI,                   KC_SPC,  KC_ENT,                    TG(_L1), TG(_L2), TG(_L3)
   ),
+
+    /* Layer 1
+  *
+  * ,---------+------+------+------+------+------+------+------+------+------+------+-------------.
+  * |   `/~   |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |    TRNS     |
+  * |---------`------`------`------`------`------`------`------`------`------`------`-------------|
+  * |   TRNS   |   \  |   '  |   -  |   =  |   [  |   ]  |   {  |   }  |   ?  |   :  |    TRNS    |
+  * |----------`------`------`------`------`------`------`------`------`------`------`------------|
+  * |   TRNS    |      |  ESC |      |      |     |       |      |   <  |   >  | TRNS |   TRNS    |
+  * |-----------`------`------`------`------`-----'-------`------`------`------`------`-----------|
+  * |   TRNS  |   TRNS  |   TRNS  |     TRNS       |     TRNS       |   TRNS  |   TRNS  |  TRNS   |
+  *  `--------+---------+---------+------^^^-------+-----^^^--------+---------+---------+---------'
+  */
   [_L1] = LAYOUT( /* LAYER 1 */
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-    _______, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, L_CURBR, R_CURBR, KC_QUES, KC_COLN, XXXXXXX,
+    _______, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, L_CURBR, R_CURBR, KC_QUES, KC_COLN, _______,
     _______, XXXXXXX, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LABK, KC_RABK, _______, _______,
-    _______, _______, _______,                   _______, _______,                   _______, XXXXXXX, XXXXXXX
+    _______, _______, _______,                   _______, _______,                   _______, _______, _______
   ),
+
+  /* LAYER 2
+ * ,---------+------+------+------+------+------+------+------+------------+------+------------.
+ * |    0    |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |    TRNS    |
+ * |---------`------`------`------`------`------`------`------`------`------`------`------------|
+ * |   TRNS   |   |  |   "  |   _  |   +  |  {   |  }   |   4  |   5  |   6  |      |    TRNS   |
+ * |----------`------`------`------`------`------`------`------`------`------`------`-----------|
+ * |   TRNS    | MUTE | VOLD | VOLU |      |      |       |   1  |   2  |   3  |      |  TRNS   |
+ * |-----------`------`------`------`------`------`-------`------`------`------`------`---------|
+ * |   TRNS  |   TRNS  |   TRNS  |     TRNS       |     TRNS       |         |         |        |
+ *  `--------+---------+---------+------^^^-------+-----^^^--------+---------+---------+--------'
+ */
   [_L2] = LAYOUT( /* LAYER 2 */
-    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_PIPE, KC_DQUO, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX,
+    KC_0   , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    _______, KC_PIPE, KC_DQUO, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,    KC_6,    XXXXXXX, _______,
     _______, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    XXXXXXX, _______,
-    _______, _______, _______,                   _______, _______,                   XXXXXXX, _______, XXXXXXX
+    _______, _______, _______,                   _______, _______,                   _______, _______, _______
   ),
+
+
+  /* LAYER 3
+  * ,---------+------+------+------+------+------+------+------+------+------+------+-------------.
+  * |  RESET  |  F1  |  F2  |  F3  |  F4  |      |      |      |      |      |      |    TRNS     |
+  * |---------`------`------`------`------`------`------`------`------`------`------`-------------|
+  * |          |  F5  |  F6  |  F7  |  F8  |      |      | DOWN |  UP  | LEFT | RGHT |    TRNS    |
+  * |----------`------`------`------`------`------`------`------`------`------`------`------------|
+  * |           |  F9  |  F10 | F11  |  F12 |     |       |      |      |      |      |    TRNS   |
+  * |-----------`------`------`------`------`-----'-------`------`------`------`------`-----------|
+  * | BL_TOGG |  TRNS   |   TRNS  |      TRNS      |     TRNS       |   TRNS  |  TRNS   |  TRNS   |
+  *  `--------+---------+---------+------^^^-------+-----^^^--------+---------+---------+---------'
+  */
   [_L3] = LAYOUT( /* LAYER 3 */
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_DOWN, KC_UP,   KC_LEFT, KC_RGHT, XXXXXXX,
+    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_DOWN, KC_UP,   KC_LEFT, KC_RGHT, _______,
     _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    BL_TOGG, _______, _______,                   _______, _______,                   XXXXXXX, XXXXXXX, _______
+    BL_TOGG, _______, _______,                   _______, _______,                   _______, _______, _______
   )
 };
 
@@ -58,12 +113,26 @@ void persistent_default_layer_set(uint16_t default_layer) {
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
+      if (!eeconfig_is_enabled()) {
+        eeconfig_init();
+      }
+
+      bool use_cmd = true;    // Use, for example, Cmd-Tab, Cmd-C, Cmd-V, etc.
+      // Compare to MAGIC_SWAP_ALT_GUI and MAGIC_UNSWAP_ALT_GUI configs, set in:
+      // quantum/quantum.c
+      if(keymap_config.swap_lalt_lgui == 1 && keymap_config.swap_ralt_rgui == 1) {
+        use_cmd = false;      // ... or, Alt-Tab, Ctrl-C, Ctrl-V, etc.
+      }
+
       switch(id) {
         case _QW:
           if (record->event.pressed) {
             persistent_default_layer_set(1UL<<_QW);
           }
           break;
+        case KC_CMD_TAB:
+          if(use_cmd) { return (record->event.pressed ? MACRO( D(LGUI),  D(TAB), END ) : MACRO( U(TAB), END )); }
+          else        { return (record->event.pressed ? MACRO( D(LALT),  D(TAB), END ) : MACRO( U(TAB), END )); }
       }
     return MACRO_NONE;
 };
