b1ec7173d581d12c01f7c1963393236eb3c5230e
kevin_tucker2 <kevin_tucker2@comcast.com>
2019-04-04 20:23:46 -0600

    keymaps update

--- keyboards/thevankeyboards/minivan/keymaps/kwtucker4/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/kwtucker4/keymap.c
@@ -29,20 +29,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty
   *
   * ,---------+------+------+------+------+------+------+------+------+------+------+-------------.
-  * |   Tab   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   Bksp      |
+  * |   Tab   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |     Bksp    |
   * |---------`------`------`------`------`------`------`------`------`------`------`-------------|
   * |   OSL_1  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |    OSL_1   |
   * |----------`------`------`------`------`------`------`------`------`------`------`------------|
   * | Shift/Esc |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   Shift   |
   * |-----------`------`------`------`------`-----'-------`------`------`------`------`-----------|
-  * |   Ctrl  |   LALT  |   GUI   |      Space     |     Enter      |   OSL_3 |   TG_2  |  TG_3   |
+  * |   Ctrl  |   LALT  |   GUI   |      Space     |     Enter      |   OSL_3 |   DF_0  |  Ctrl   |
   *  `--------+---------+---------+------^^^-------+-----^^^--------+---------+---------+---------'
   */
   [_QW] = LAYOUT( /* Qwerty */
     KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     OSL(_L1), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, OSL(_L2),
     SFT_ESC,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,
-    KC_LCTL,  KC_LALT	, KC_LGUI,                   KC_SPC,  KC_ENT,                    OSL(_L3), TG(_L2), DF(_QW)
+    KC_LCTL,  KC_LALT	, KC_LGUI,                   KC_SPC,  KC_ENT,                    OSL(_L3), DF(_QW), KC_RCTL
   ),
 
   /* Layer 1
