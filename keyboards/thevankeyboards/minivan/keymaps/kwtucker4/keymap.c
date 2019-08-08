0641bf5e2698fec9026edbc86e74f4b48f60ad80
kevin_tucker2 <kevin_tucker2@comcast.com>
2019-08-08 10:36:03 -0600

    update to keymap

--- keyboards/thevankeyboards/minivan/keymaps/kwtucker4/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/kwtucker4/keymap.c
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_QW] = LAYOUT( /* Qwerty */
-    KC_TAB,   KC_Q,     KC_W,    		   KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_BSPC,
-    OSL(_L1), KC_A,     KC_S,    		   KC_D,    KC_F,  KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN, OSL(_L2),
-    SFT_ESC,  KC_Z,     KC_X,    		   KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSHIFT,
-    KC_LCTL,  KC_LALT	, KC_LGUI,                TD_GS, KC_ENT,                              OSL(_L3), DF(_QW), KC_RCTL
+    KC_TAB,    KC_Q,     KC_W,    		   KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_BSPC,
+    KC_LCTL,   KC_A,     KC_S,    		   KC_D,    KC_F,  KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN, OSL(_L2),
+    KC_LSHIFT, KC_Z,     KC_X,    		   KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSHIFT,
+    OSL(_L1),  KC_LALT,  KC_LGUI,                 KC_SPC,KC_ENT,                              OSL(_L3), DF(_QW), KC_RCTL
   ),
 
 
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_L2] = LAYOUT( /* LAYER 2 */
     KC_0   , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_DQUO, KC_PIPE, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,    KC_6,    XXXXXXX, _______,
+    KC_ESC , KC_DQUO, KC_PIPE, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,    KC_6,    XXXXXXX, _______,
     _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MRWD, KC_MPLY, KC_MFFD, KC_1,    KC_2,    KC_3,    XXXXXXX, _______,
     _______, _______, _______,                   _______, _______,                   _______, _______, _______
   ),
